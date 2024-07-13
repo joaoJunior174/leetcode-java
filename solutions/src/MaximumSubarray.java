b9086b160b1f73dc6720687a80d61f3609c702e7
João Aurélio Francisco Junior <jafrancisco@rd.com.br>
2024-07-12 21:50:52 -0300

    solucao O(n) para MaximumSubarray

--- solutions/src/MaximumSubarray.java
+++ solutions/src/MaximumSubarray.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class MaximumSubarray {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(generateSolution(new int[]{-1,0,-1}));
+    }
+
+    public static int generateSolution(int [] nums) {
+
+        int currentSum = nums[0];
+        int bestSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int nextSum = currentSum + nums[i];
+            currentSum = nums[i];
+
+            if (nextSum > nums[i]) {
+                currentSum = nextSum;
+            }
+
+            if (currentSum > bestSum) {
+                bestSum = currentSum;
+            }
+        }
+
+        return bestSum;
     }
 }
